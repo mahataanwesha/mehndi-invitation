8a003995037d6ad6feadaad994389a106df51d05
mahataanwesha <mahataanwesha35@gmail.com>
2025-10-04 11:37:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // ✅ important for correct relative paths on Vercel
+  build: {
+    outDir: "dist", // Vercel expects this
+  },
 });
